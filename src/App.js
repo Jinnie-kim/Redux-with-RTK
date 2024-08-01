6337a1caadd99c967431c3ae2f4de1bad901e88e
Jinnie <hjk7639@gmail.com>
2024-08-01 14:05:00 +0900

    Resetting State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useDispatch } from 'react-redux';
 import MoviePlaylist from './components/MoviePlaylist';
 import SongPlaylist from './components/SongPlaylist';
+import { reset } from './store';
 import './style.css';
 
 function App() {
-    const handleReserClick = () => {
+    const dispatch = useDispatch();
+    const handleResetClick = () => {
         //
+        dispatch(reset());
     };
 
     return (
         <div>
-            <button onClick={() => handleReserClick()} className="button is-danger">
+            <button onClick={() => handleResetClick()} className="button is-danger">
                 Reset Both Playlists
             </button>
             <hr />
